8518207c3624896cf0941bc1ff5736ec549dcf2c
Carl Hinderer <carl.hinderer4@gmail.com>
2020-12-15 15:35:15 -0700

    Rendering the index template.

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home_view():
-    return "<h1>Under Construction</h1>"
+    return render_template('index.html')
